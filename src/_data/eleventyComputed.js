68045eec7c215f7422545ba99ed469f28fbc2ec9
Sia Karamalegos <siakaramalegos@gmail.com>
2025-01-15 13:17:45 -0500

    Add events to home page, change date to local

--- src/_data/eleventyComputed.js
+++ src/_data/eleventyComputed.js
@@ -24,27 +24,6 @@ const getSpeakers = (data, talkSpeakers) => {
   })
 }
 
-const getHoursOffsetUTC = (timeZone) => {
-  const timeZoneName = Intl.DateTimeFormat("ia", {
-    timeZoneName: "short",
-    timeZone,
-  })
-    .formatToParts()
-    .find((i) => i.type === "timeZoneName").value;
-  const offset = timeZoneName.slice(3);
-  if (!offset) return 0;
-
-  const matchData = offset.match(/([+-])(\d+)(?::(\d+))?/);
-  if (!matchData) throw `cannot parse timezone name: ${timeZoneName}`;
-
-  const [, sign, hour, minute] = matchData;
-
-  return hour;
-};
-const hoursOffsetUTC = getHoursOffsetUTC("US/Eastern")
-console.log(hoursOffsetUTC);
-
-
 module.exports = {
 	events: (data) => {
     const publishedEvents = data.rawEvents.filter(event => event.content.published)
@@ -53,13 +32,6 @@ module.exports = {
       const talks = getTalks(data, event.content.talks)
 
       // Need to force UTC time parsing for the event start time
-      // "2025-01-17 12:00"
-      const dateComponents = event.content.start_time.split(' ')
-      const [year, month, day] = dateComponents[0].split('-').map(e => parseInt(e))
-      const [hours, minutes] = dateComponents[1].split(':').map(e => parseInt(e))
-
-      // Date.UTC(year, monthIndex, day, hours, minutes)
-      // const date = new Date(Date.UTC(year, month - 1, day, hours, minutes))
       const date = new Date(event.content.start_time + 'Z')
       // .toLocaleString([],{
       //   day: '2-digit',
@@ -69,8 +41,6 @@ module.exports = {
       //   minute: "numeric",
       //   timeZoneName: "short",
       // })
-      // console.log(event.content.start_time);
-      // console.log({date});
 
       return {
         ...event,
